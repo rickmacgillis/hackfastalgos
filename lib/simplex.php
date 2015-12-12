7eac05d32edf68d3884ee13d90f0115d63d9294a
Rick Mac Gillis <rick.macgillis@rickmacgillis.com>
2015-12-11 20:47:11 -0500

    Added Simplex skeleton
    Signed-off-by: Rick Mac Gillis <rick.macgillis@rickmacgillis.com>

---
+++ lib/simplex.php
@@ -0,0 +1,16 @@
+<?HH
+/**
+ * @author Rick Mac Gillis
+ *
+ * Herpilies! Herpilies! [https://www.youtube.com/watch?v=PxYQQoYfMtQ]
+ * This Simplex algorithm is a radmentary LP solver.
+ *
+ * Learn more @link http://algs4.cs.princeton.edu/65reductions/LinearProgramming.java.html
+ */
+
+namespace HackFastAlgos;
+
+class Simplex
+{
+	
+}
